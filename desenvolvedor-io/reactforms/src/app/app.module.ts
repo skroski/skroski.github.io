f86ccd06abdbadf2757709e0aea378addd0aa0ea
daniel.skroski <daniel@duplod.com.br>
2021-07-31 14:40:18 -0300

    Teste Dev.io

---
+++ desenvolvedor-io/reactforms/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { rootRouterConfig } from './app.routes';
+import { CadastroComponent } from './cadastro/cadastro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    SobreComponent,
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
+  ],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
